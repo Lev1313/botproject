1b6f7241cff89b827a22ac361e3955a2e2a55fa7
Lev1313 <76059863+Lev1313@users.noreply.github.com>
2022-01-16 20:45:00 +0300

    main1.1.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # -*- coding: utf8 -*-
 import telebot
 import requests
+import time
 import random
 # Указываем токен
-bot = telebot.TeleBot('1592617905:AAEt8WNuHnMWChAGUfW_aOGFab269DNGwjk')
+
+bot = telebot.TeleBot('1592617905:AAEt8WNuHnMWChAGUfW_aOGFab269DNGwjk', threaded=False)
 # Импортируем типы из модуля, чтобы создавать кнопки
 from telebot import types
 # Метод, который получает сообщения и обрабатывает их
@@ -63,53 +65,60 @@ def get_text_messages(message):
 def callback_worker(call):
     # Если нажали на одну из 12 кнопок — выводим гороскоп
     if call.data == "Pisces":
-        answ = requests.get('http://ohmanda.com/api/horoscope/pisces').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/pisces/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Aries":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Aries').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/aries/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Taurus":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Taurus').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/taurus/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Gemini":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Gemini').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/gemini/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Cancer":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Cancer').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/cancer/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Leo":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Leo').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/leo/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Virgo":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Virgo').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/virgo/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Libra":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Libra').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/libra/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Scorpio":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Scorpio').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/scorpio/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Sagittarius":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Sagittarius').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/sagittarius/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Capricorn":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Capricorn').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/capricorn/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
     elif call.data == "Aquarius":
-        answ = requests.get('http://ohmanda.com/api/horoscope/Aquarius').json()
+        answ = requests.get('https://ohmanda.com/api/horoscope/aquarius/').json()
         bot.send_message(call.message.chat.id, answ['horoscope'])
 
 
 # Запускаем постоянный опрос бота в Телеграме
-bot.polling(none_stop=True, interval=0)
+while True:
+    try:
+        bot.polling(none_stop=True)
+
+    except Exception as e:
+        print(e)  # или просто print(e) если у вас логгера нет,
+        # или import traceback; traceback.print_exc() для печати полной инфы
+        time.sleep(15)
